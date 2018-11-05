79818b7a0b7835b139532d1b5b1a40f887b2255a
Iwan Siauw <evansiauw@gmail.com>
2018-11-05 16:37:06 -0500

    Initial Commit

---
+++ MoonHeart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoonHeart
+//
+//  Created by Iwan Siauw on 11/5/18.
+//  Copyright © 2018 Iwan Siauw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MoonHeartTests/MoonHeartTests.swift
@@ -0,0 +1,34 @@
+//
+//  MoonHeartTests.swift
+//  MoonHeartTests
+//
+//  Created by Iwan Siauw on 11/5/18.
+//  Copyright © 2018 Iwan Siauw. All rights reserved.
+//
+
+import XCTest
+@testable import MoonHeart
+
+class MoonHeartTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
