1f47af71e486538a47ae2874c983e4a65cc4cea9
Iwan Siauw <evansiauw@gmail.com>
2019-01-01 11:55:04 -0500

    testing first commit on new branch

---
+++ MoonHeart/Supporting Files/mainViewController.swift
@@ -0,0 +1,38 @@
+//
+//  mainViewController.swift
+//  MoonHeart
+//
+//  Created by Iwan Siauw on 12/17/18.
+//  Copyright © 2018 Iwan Siauw. All rights reserved.
+//
+
+import UIKit
+
+
+class mainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        
+    }
+    
+    func style(){
+        
+        let attrs = [
+            NSAttributedString.Key.foregroundColor: UIColor.white,
+            NSAttributedString.Key.font: UIFont(name: "PartyLetPlain", size: 30)!
+        ]
+        
+        navigationController?.navigationBar.titleTextAttributes = attrs
+        
+        navigationController?.navigationBar.barTintColor = UIColor.black
+        setNeedsStatusBarAppearanceUpdate()
+        
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+}
